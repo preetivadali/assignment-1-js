00496a5725e0407c6a2238deb39a42a7b89eb230
Preeti Vadali <preeti.abhiram@gmail.com>
2024-10-14 23:18:15 +0200

    basic javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+// Q-1
+console.log("Hello World");
+
+// Q-2
+
+let name ="Preeti";
+let age ="34";
+let color ="Blue";
+
+console.log(`My Name is  ${name}, I am ${age}  , my favorite color is  ${color}`);
+
+
+// Q-3
+
+let x = 10;
+let y = 15;
+
+console.log(x+y);
+console.log(y-x);
+console.log(x*y);
+console.log(y/x);
